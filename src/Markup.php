de62907e02f115d6082fe237a5f0a2d712ed0019
David Persson <davidpersson@gmx.de>
2012-11-09 06:40:48 +0100

    Require and use dependencies early; all static methods.

--- src/Markup.php
+++ src/Markup.php
@@ -2,6 +2,12 @@
 
 namespace markup;
 
+require_once __DIR__ . '/markdown.php';
+require_once __DIR__ . '/smartypants.php';
+
+use MarkdownExtra_Parser;
+use SmartyPants_Parser;
+
 class Markup {
 
     public static $settings = array(
@@ -10,7 +16,7 @@ class Markup {
 
     protected static $_loaded = array();
 
-	public function parse($content, $pass = array()) {
+	public static function parse($content, $pass = array()) {
 		if (!$pass) {
 			$pass = static::$settings['pass'];
 		}
@@ -20,12 +26,12 @@ class Markup {
 		return $content;
     }
 
-	public function normalize($content) {
+	public static function normalize($content) {
 		return str_replace("\r\n", "\n", $content);
 	}
 
 	// No pre and code blocks through indentation.
-	public function easy($content) {
+	public static function easy($content) {
 		$content = explode("\n", $content);
 
 		foreach ($content as &$line) {
@@ -37,23 +43,21 @@ class Markup {
 	// markdown -> markdown
 	// @link http://github.github.com/github-flavored-markdown/
 	// @link http://github.com/mojombo/github-flavored-markdown
-	public function gfm($content) {
+	public static function gfm($content) {
 		return preg_replace('/(?<!\n)\n(?![\n\*\#\-])/', "  \n", $content);
 	}
 
 	// markdown -> html
-	public function markdown($content) {
+	public static function markdown($content) {
 		if (!isset(static::$_loaded['markdown'])) {
-			require_once 'markdown.php';
-			statci::$_loaded['markdown'] = new MarkdownExtra_Parser();
+			static::$_loaded['markdown'] = new MarkdownExtra_Parser();
 		}
 		return static::$_loaded['markdown']->transform($content);
 	}
 
 	// markdown/html -> html
-	public function smartyPants($content) {
+	public static function smartyPants($content) {
 		if (!isset(static::$_loaded['smartyPants'])) {
-			require_once 'smartypants.php';
 			static::$_loaded['smartyPants'] = new SmartyPants_Parser();
 		}
 		return static::$_loaded['smartyPants']->transform($content);
