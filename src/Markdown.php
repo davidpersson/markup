a4fcf413bff395b371f886f7f2f2f64ff171efcc
David Persson <davidpersson@gmx.de>
2013-02-07 11:03:54 +0100

    Documenting class.

--- src/Markdown.php
+++ src/Markdown.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * markup
+ *
+ * Copyright (c) 2012-2013 David Persson
+ *
+ * Distributed under the terms of the MIT License.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright  2011-2013 David Persson <nperson@gmx.de>
+ * @license    http://opensource.org/licenses/mit-license The MIT License
+ * @link       http://github.com/davidpersson/markdown
+ */
 
 namespace markup;
 
@@ -10,23 +22,45 @@ use MarkdownExtra_Parser;
 use SmartyPants_Parser;
 use MarkdownExtraExtended_Parser;
 
-class Markup {
+class Markdown {
 
+	/**
+	 * Parser steps to pass content through.
+	 *
+	 * @var array
+	 */
     protected static $_pass = array(
 		'normalize',
 		'easy',
 		'gfm',
-		// 'markdownExtra',
 		'markdownExtraExtended',
 		'smartyPants'
 	);
 
+	/**
+	 * Holds instances of already loaded parsers.
+	 *
+	 * @var array
+	 */
     protected static $_loaded = array();
 
+	/**
+	 * Allows to configure the steps content is run through.
+	 *
+	 * @param array $config An array of steps.
+	 * @return void
+	 */
 	public static function config(array $config = array()) {
 		return static::$_pass = $config;
 	}
 
+	/**
+	 * Parse markdown to HTML.
+	 *
+	 * @param string $content The markdown content to parse.
+	 * @param array $config An array of steps.
+	 * @return string HTML
+	 */
 	public static function parse($content, array $pass = array()) {
 		if (!$pass) {
 			$pass = static::$_pass;
@@ -41,11 +75,26 @@ class Markup {
 		return $content;
     }
 
+	/* Pre-Parsers */
+
+	/**
+	 * Normalizes line endings to \n.
+	 *
+	 * @param string $content The markdown content to parse.
+	 * @return string The markdown content with normalized line endings.
+	 */
 	public static function normalize($content) {
 		return str_replace("\r\n", "\n", $content);
 	}
 
-	// No pre and code blocks through indentation.
+	/**
+	 * Simplifies certain markdown features. Currently strips away assumed
+	 * unintended indentation. By applying this step you loose any pre/code
+	 * block formatting.
+	 *
+	 * @param string $content The markdown content to parse.
+	 * @return string Markdown content.
+	 */
 	public static function easy($content) {
 		$content = explode("\n", $content);
 
@@ -55,33 +104,59 @@ class Markup {
 		return implode("\n", $content);
 	}
 
-	// markdown -> markdown
-	// @link http://github.github.com/github-flavored-markdown/
-	// @link http://github.com/mojombo/github-flavored-markdown
+	/**
+	 * Applies a subset of GitHub flavored markdown.
+	 *
+	 * @link http://github.github.com/github-flavored-markdown/
+	 * @param string $content The markdown content to parse.
+	 * @return string Markdown content.
+	 */
 	public static function gfm($content) {
 		return preg_replace('/(?<!\n)\n(?![\n\*\#\-])/', "  \n", $content);
 	}
 
-	// markdown -> html
+	/* Parsers */
+
+	/**
+	 * Parses markdown using the markdownExtra parser
+	 * and transforms it into HTML.
+	 *
+	 * @param string $content The markdown content to parse.
+	 * @return string The content tranformed into HTML.
+	 */
 	public static function markdownExtra($content) {
 		if (!isset(static::$_loaded[__FUNCTION__])) {
 			static::$_loaded[__FUNCTION__] = new MarkdownExtra_Parser();
 		}
 		return static::$_loaded[__FUNCTION__]->transform($content);
-}
+	}
 
-	// markdown/html -> html
-	public static function smartyPants($content) {
+	/**
+	 * Parses markdown using the markdownExtraExtended
+	 * parser and transforms it into HTML.
+	 *
+	 * @param string $content The markdown content to parse.
+	 * @return string The content tranformed into HTML.
+	 */
+	public static function markdownExtraExtended($content) {
 		if (!isset(static::$_loaded[__FUNCTION__])) {
-			static::$_loaded[__FUNCTION__] = new SmartyPants_Parser();
+			static::$_loaded[__FUNCTION__] = new MarkdownExtraExtended_Parser();
 		}
 		return static::$_loaded[__FUNCTION__]->transform($content);
 	}
 
-	// markdown/html -> html
-	public static function markdownExtraExtended($content) {
+	/* Post-Parser */
+
+	/**
+	 * Parses markdown (or post-process HTML) using the smartyPants parser
+	 * and transforms it into HTML.
+	 *
+	 * @param string $content The markdown or HTML content to parse.
+	 * @return string The content tranformed into HTML.
+	 */
+	public static function smartyPants($content) {
 		if (!isset(static::$_loaded[__FUNCTION__])) {
-			static::$_loaded[__FUNCTION__] = new MarkdownExtraExtended_Parser();
+			static::$_loaded[__FUNCTION__] = new SmartyPants_Parser();
 		}
 		return static::$_loaded[__FUNCTION__]->transform($content);
 	}
